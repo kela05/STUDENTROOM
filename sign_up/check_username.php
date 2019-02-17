f09c18c9fd00c9d7e17864b5a17195308615918e
megimysliu <46091151+megimysliu@users.noreply.github.com>
2019-02-17 13:07:47 +0100

    Add files via upload

---
+++ sign_up/check_username.php
@@ -0,0 +1,86 @@
+<?php
+
+include 'dbconn.php';
+
+if(isset($_POST['username_check'])){
+    
+    
+    $username=mysqli_real_escape_string($conn,$_POST['username']);
+    $query = "SELECT user_id FROM users WHERE  username='$username'";
+    $result= mysqli_query($conn, $query);
+    if(!$result){
+        echo"alert('Couldn't proccess request.Try again)";
+    }else{
+        if(mysqli_num_rows($result)>0){
+            echo'0';
+        }else{
+            echo '1';   
+        }
+    }
+    exit();
+    }
+   
+    if(isset($_POST['email_check'])){
+    
+    $email=mysqli_real_escape_string($conn,$_POST['email']);
+    $query = "SELECT user_id FROM users WHERE  email='$email'";
+    $result= mysqli_query($conn, $query);
+    if(!$result){
+        echo"alert('Couldn't proccess request.Try again)";
+    }else{
+        if(mysqli_num_rows($result)>0){
+            echo'0';
+        }else{
+            echo '1';   
+        }
+    }
+    exit();
+    }
+
+    
+    if(isset($_POST['submit'])){
+        
+       $username=mysqli_real_escape_string($conn,$_POST['username']);
+       $email=mysqli_real_escape_string($conn,$_POST['email']);
+       $password=mysqli_real_escape_string($conn,$_POST['password']);
+       $password=hash("ripemd128",$password);
+       $com_code = md5(uniqid(rand()));
+          $qwr = "INSERT INTO users(username,email,password,token) VALUES('$username','$email','$password','$com_code')";
+           $rsl = mysqli_query($conn, $qwr);
+         if(!$rsl){ 
+             
+             echo"exists";
+             exit();
+         }
+             else{
+    
+                     echo 'success';
+                 $to = $email;
+                 $subject = 'Signup|Verification';
+                 $message = "
+                     Thanks for signing up!
+                     Your account has been created,you can login after activating your account by pressing the URL below.
+                     
+                    ---------------------
+                     Username: '.$username'
+                     Password: '.$password'
+                    
+                    ----------------------
+                    
+                    Please click the link to activate your account:
+                    
+                     http://localhost/PhpProject1/verification.php?email=$email&com_code=$com_code";
+                 $headers = 'From:noreply@studentroom.com'."\r\n";
+                 if(mail($to,$subject,$message,$headers)){
+                  
+                 header("Location: http://localhost/PhpProject1/success.php");
+                 exit();
+             }else{
+                 echo "<script>alert('An error occurred!Couldn't send email,please try again.)</script>";
+             }
+             }
+        
+       
+        
+    }
+    mysqli_close($conn);
---
+++ sign_up/verification.php
@@ -0,0 +1,70 @@
+<?php include "navbar.php"; ?>
+<?php include "header.html";?>
+<?php  include "dbconn.php"?>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Verification</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="stylee.css">
+  <link rel="stylesheet" href="welcome.css">
+  <link rel="stylesheet" href="StyleSr.css">
+  
+  <style>
+      h1{
+          margin-top: 100px;
+          margin-left: 50px;
+          color:white;
+      }
+  </style>
+    </head>
+    <body>
+        <?php
+        if(!$conn){
+    die("Connection error: ".mysqli_connect_error());
+}
+
+     function valid($data){
+                    $data=trim(stripslashes(htmlspecialchars($data)));
+                    return $data;
+                }
+                
+      if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['com_code']) && !empty($_GET['com_code'])){
+    // Verify data
+    $email = mysqli_real_escape_string($conn,valid($_GET['email'])); 
+    $com_code = mysqli_real_escape_string($conn,$_GET['com_code']); 
+      $qwry = "SELECT user_id FROM users WHERE email='$email' AND token='$com_code' AND active=0";           
+    $search = mysqli_query($conn,"SELECT user_id FROM users WHERE email='$email' AND token='$com_code' AND active=0") or die(mysqli_error($conn)); 
+    $match  = mysqli_num_rows($search);
+                 
+    if($match > 0){
+        // We have a match, activate the account
+        $qweryy= "UPDATE `users` SET `active`=1 WHERE email='".$email."' AND token='".$com_code."' AND active=0";
+        mysqli_query($conn,$qweryy) or die(mysqli_error($conn));
+        $qry="UPDATE users SET token='' WHERE token='$com_code'";
+        $res=mysqli_query($conn,$qry);
+        if(mysqli_affected_rows($conn)>0){
+           
+        echo '<h1 style="color:white">Your account has been activated, you can now <a href="http://localhost/PhpProject1/login.php">login here</a></h1>';
+        }
+        
+        }else{
+        // No match -> invalid url or account has already been activated.
+        echo '<h1>The url is either invalid or you already have activated your account.</h1>';
+    }
+                 
+}else{
+    // Invalid approach
+    echo '<h1>Invalid approach, please use the link that has been send to your email.</h1>';
+} 
+mysqli_close($conn);
+        ?>
+    </body>
+</html>
