f07f819603c9d352a1082df52233412ac4e773c2
xhena <46090753+xhena@users.noreply.github.com>
2019-02-26 14:19:57 +0100

    Add files via upload

---
+++ notification.php
@@ -0,0 +1,49 @@
+<?php include"../nav_head/navbar.html" ?>
+
+<html>
+    <head>
+        
+    </head>
+
+    <body>
+<!--      <div class="wrapper"> -->
+
+        <div class="alert alert-block">
+  <a class="close" data-dismiss="alert"></a>
+  <h4 class="alert-heading"><strong>27th Conference EcoTER 2019 - First call</strong></h4>
+  <pre>
+Thirrje per pjesemarrje ne konferencen nderkombetare EcoTer. 
+2019 tek stafi juaj akademik si dhe studentet e Masterit Shkencor dhe Doktoratures qe mund te kene interes te marrin pjese ne te. Njoftimi te shpallet <br>edhe ne website-in e FSHN-se. 
+Konferenca zhvillohet ne Serbi, ne datat 18-21 Qershor, 2019.
+</pre>
+</div>
+
+<div class="alert alert-success">
+  <a class="close" data-dismiss="alert"></a>
+    <h4 class="alert-heading"><strong>APLIKIM NË PROGRAMIN KOMBËTAR TË PRAKTIKAVE TË PUNËS THIRRJA IV</strong></h4>
+  <pre>
+Në mbështetje të vendimit të Këshillit të Ministrave nr. 395, datë 29.04.2015 “Për miratimin e programit kombëtar të praktikave të punës <br>në administratën shtetëroredhe institucionet e tjera publike”, i ndryshuar, Ministria e Arsimit, Sportit dhe Rinisë shpall hapjen e thirrjes <br>së katërt të  Programit Kombëtar të Praktikave të Punës.
+Afati për aplikim është nga data 8 shkurt deri më datë 28 shkurt 2019.
+Aplikimi është i hapur për të rinjtë profesionistë nga mosha 21 deri 26 vjeç, të cilët zoterojnë minimalisht një diplomë Bachelor. </pre>
+        <br>
+</div>
+
+<div class="alert alert-error">
+  <a class="close" data-dismiss="alert"></a>
+     <h4 class="alert-heading"><strong>Mundesi aplikimi per bursa Erasmus + per studentet dhe stafik akademik ne Universitetin Politeknik te Barit, Itali</strong></h4>
+<pre>
+Ne kuader  te programit Erasmus + eshte hapur thirrja per aplikime per bursa mobiliteti per studente dhe stafin akademik<br>ne Universitetin Politeknik te Barit, Itali, per semestrin e dyte  te vitin akademik 2018-2019.Afati i fundit per aplikim <br>eshte 27 Shkurt, 2019.Dokumentat duhen dorezuar dorazi ne Rektoratin e Universitetit te Tiranes, Drejtoria e Marredhenie <br>me Jashte dhe Studentet, Sektori  per Marredhenie me Jashte,  si dhe te dergohen te skanuara ne e-mailin iro@unitir.edu.al </pre>
+</div>
+
+<div class="alert alert-info">
+  <a class="close" data-dismiss="alert"></a>
+    <h4 class="alert-heading"><strong>Mundesi aplikimi per bursa shkembimi per studente dhe stafin akademik ne Universitetin Trakya ne Turqi</strong></h4>
+ <pre>
+Ne kuader te programit Mevlana Exchange eshte hapur thirrja per aplikime per bursa shkembimi per studentet dhe stafin akademik per mesimdhenie <br>ne Universitetin Trakya ne Turqi, per vitin akademik 2019-2020.Afati i fundit per te aplikuar eshte data 28 Shkurt, 2019.
+Dokumentacioni dorezohet dorazi ne Rektoratin e UT-se, Drejtoria e marredhenieve me Jashte dhe Studente, Sektori per Marredhenie me Jashte si<br> dhe dergohet i skanuar permes postes elektronike ne adresen iro@unitir.edu.al
+</pre>
+</div>
+<!--        </div>-->
+
+</body>
+</html>
